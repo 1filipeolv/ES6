80d67d6ddefb43bfd6c808272df8e85091244f60
1filipeolv <filipeolv.lima@gmail.com>
2025-07-15 11:53:15 -0300

    1filipeolv

---
+++ Improvements/ShortHand.js
@@ -0,0 +1,17 @@
+    let firstName = 'Willian';
+    let surname = 'Justen';
+    let age = 26;
+
+    const person = {
+        firstName,
+        surname,
+        age,
+
+        hello() {
+            console.log('Hello!')
+        }
+    };
+
+    console.log(person);
+
+    person.hello();
